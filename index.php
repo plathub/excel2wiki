5169fed344dd6fdce5bbd74eddb6b6eae0886a0b
Stephan Plath <plathine@gmail.com>
2017-03-30 10:27:13 +0200

    general support for converting csv data to wiki markup; css style

--- index.php
+++ index.php
@@ -24,46 +24,58 @@ OTHER DEALINGS IN THE SOFTWARE.
 -->
 
 <?php
+$data = trim($_POST['data']);
 echo "<html>
-<head><title>excel2wiki.net | Excel xls to wiki copy and paste converter for wikipedia and mediawiki</title></head>
-<body><h1>Copy & Paste Excel-to-Wiki Converter</h1>
+<head><title>excel2wiki</title>
+<link rel='stylesheet' href='style.css' type='text/css' />
+</head>
+<body><h2>Copy &amp; Paste Excel-to-Wiki Converter</h2>
+<p>Copy &amp; paste CSV data or cells from Excel and click submit. Paste results into Wikipedia or similar Wiki.</p>
 <form action='index.php' method='post'>
-  <textarea name='data' rows='20' cols='100'></textarea><br>
-  <label for='tableclass'>Table style:
-  <input id='tableclass' type='text' name='tableclass' value='class=\"wikitable\"'/><br/>
+  <textarea name='data' rows='20' cols='100'>$data</textarea><br/><br/>
+  <label for='delimiter'>Delimiter:
+  <select id='delimiter' name='delimiter'>
+  <option value='\t' selected>Tab (Excel)</option>
+  <option value=';'>Semicolon</option>
+  <option value=','>Comma</option>
+  <option value=' '>Space</option>
+  </select>
+  </label>
+  <label for='tableclass'>Wiki table style:
+  <select id='tableclass' name='tableclass'><option></option><option selected>wikitable</option><option>wikitable sortable</option></select>
   </label>
   <input type='checkbox' name='header' checked='checked'>Format first row as header<br/><br/>
-  <input type='submit' />
+  <button>Convert Text to Wiki Markup</button>
 </form>";
 
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-echo "<h2>Instructions</h2>
-<p>Copy & paste cells from Excel and click submit. Paste results into wikipedia or similar wiki.</p>";
-} 
-else {
-	echo "<h2>Result</h2>\n<pre>\n{| ". $_POST['tableclass'] . "\n";
-	$lines = preg_split("/\n/", $_POST['data']);
-	$n = sizeof($lines);
-	foreach ($lines as $index => $value) 
+if ($data != '')
+{
+	$delimiter = $_POST['delimiter'];
+	$tableclass = $_POST['tableclass'];
+	$first_row_as_header = isset($_POST['header']);
+	echo "<h2>Wiki Markup</h2>\n<pre>\n{| ";
+	if ($tableclass != '')
 	{
-	  $line = preg_split("/\t/", $value);
-	  if ($index == 0 && isset($_POST['header'])) 
-	  {
-		$separator = '!';
-	  }
-	  else {
-		$separator = '|';
-	  }
-	  $data = implode($separator.$separator, $line);
-	  echo $separator .' '. $data;
-	  if ($index < $n - 1) 
-	  {
-		echo "|-\n";
-	  }
+		$tableclass = 'class="'. $tableclass .'"';
 	}
-    echo "\n|}</pre>";
+	echo $tableclass ."\n";
+	$lines = preg_split("/\n/", $data);
+	$n = sizeof($lines);
+	foreach ($lines as $index => $line) 
+	{
+		$separator = ($index == 0 && $first_row_as_header) ? '!' : '|';
+		$line = str_replace($separator, "&lt;nowiki&gt;$separator&lt;/nowiki&gt;", $line);
+		$values = preg_split('/'.$delimiter.'/', $line);
+		$data = implode(" $separator$separator ", $values);
+		echo $separator .' '. $data;
+		if ($index < $n - 1) 
+		{
+			echo "|-\n";
+		}
+	}
+	echo "\n|}</pre>";
 }
-
+echo "<footer><a href='https://github.com/plathub/excel2wiki'>Excel2Wiki Copy and Paste Wiki Converter</a></footer>";
 echo "</body></html>";
 
 ?>
